f83f1ab9561fbc343af0aedcc7b502bbe87b74e6
TwolDE <awhalley@waitrose.com>
2016-11-07 16:59:44 +0100

    HD51 revert backup restriction

--- src/ImageManager.py
+++ src/ImageManager.py
@@ -352,12 +352,9 @@ class VIXImageManager(Screen):
 			self.keyBackup()
 
 	def keyBackup(self):
-		if getMachineBuild() in ('vusolo4k','hd51','hd52'):
-			self.session.open(MessageBox, _("Sorry, But this machine is not supported yet."), MessageBox.TYPE_INFO, timeout=10)
-		else:
-			message = _("Are you ready to create a backup image ?")
-			ybox = self.session.openWithCallback(self.doBackup, MessageBox, message, MessageBox.TYPE_YESNO)
-			ybox.setTitle(_("Backup Confirmation"))
+		message = _("Are you ready to create a backup image ?")
+		ybox = self.session.openWithCallback(self.doBackup, MessageBox, message, MessageBox.TYPE_YESNO)
+		ybox.setTitle(_("Backup Confirmation"))
 
 	def doBackup(self, answer):
 		if answer is True:
