bdd3eb30e0860aaf5bef5f090655c1d57c23a5c5
twol <tonywhalley@t-online.de>
2017-05-23 13:54:27 +0200

    [h7] add bootloader version for multiboot

--- src/HD51MultiBoot.py
+++ src/HD51MultiBoot.py
@@ -185,7 +185,7 @@ class HD51MultiBoot(Screen):
 		'''
 
 		self.optionsList = (('boxmode=1', _('2160p60 without PiP (Standard)')), ('boxmode=12', _('2160p50 with PiP (Experimental)')))
-		self.bootloaderList = ('v1.07-r19', 'v1.07-r21')
+		self.bootloaderList = ('v1.07-r19', 'v1.07-r21', 'v1.07-r35')
 
 		#for compatibility to old or other images set 'self.enable_bootnamefile = False'
 		#if 'False' and more as on file with same kernel exist is possible no exact matching found (only to display)
