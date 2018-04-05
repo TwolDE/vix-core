3a91745452f27e37c26070133df5d32a3db8c4db
Tony Whalley <awhalley@waitrose.com>
2018-04-05 15:48:55 +0200

    change default on delete

--- src/ImageManager.py
+++ src/ImageManager.py
@@ -318,7 +318,7 @@ class VIXImageManager(Screen):
 		self.sel = self['list'].getCurrent()
 		if self.sel:
 			message = _("Are you sure you want to delete this image backup:\n ") + self.sel
-			ybox = self.session.openWithCallback(self.doDelete, MessageBox, message, MessageBox.TYPE_YESNO, default=False)
+			ybox = self.session.openWithCallback(self.doDelete, MessageBox, message, MessageBox.TYPE_YESNO, default=True)
 			ybox.setTitle(_("Remove confirmation"))
 		else:
 			self.session.open(MessageBox, _("You have no image to delete."), MessageBox.TYPE_INFO, timeout=10)
