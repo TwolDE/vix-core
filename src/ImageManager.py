ad7ccab2608dae138f011acb13413266b5f68df6
Tony Whalley <awhalley@waitrose.com>
2018-04-27 16:11:11 +0200

    ImageManager - simplify multiboot

--- src/ImageManager.py
+++ src/ImageManager.py
@@ -492,7 +492,7 @@ class VIXImageManager(Screen):
 				if 'coherent_poll=2M' in open("/proc/cmdline", "r").read():
 					WriteStartup(slot, self.ReExit)
 				else:
-					startupFileContents = "boot emmcflash0.kernel%s 'brcm_cma=%s brcm_cma=%s root=/dev/mmcblk0p%s rw rootwait %s_4.boxmode=1'\n" % (slot, SystemInfo["canMode12"][0], SystemInfo["canMode12"][1], slot * 2 + self.addin, model)
+					startupFileContents = "boot emmcflash0.kernel%s 'brcm_cma=%s root=/dev/mmcblk0p%s rw rootwait %s_4.boxmode=1'\n" % (slot, SystemInfo["canMode12"][0], slot * 2 + self.addin, model)
 					WriteStartup(startupFileContents, self.ReExit)
 		else:
 			self.session.openWithCallback(self.restore_infobox.close, MessageBox, _("ofgwrite error (also sent to any debug log):\n%s") % result, MessageBox.TYPE_INFO, timeout=20)
