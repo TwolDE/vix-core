01bbcb74e0b240acb9b723ea26118e5868f030b5
Tony Whalley <awhalley@waitrose.com>
2018-05-24 10:10:58 +0200

    ImageManager - Menu backup settings overrides slot backup/nobackup list

--- src/ImageManager.py
+++ src/ImageManager.py
@@ -403,10 +403,11 @@ class VIXImageManager(Screen):
 		HIslot = len(imagedict) + 1
 		currentimageslot = GetCurrentImage()
 		for x in range(1,HIslot):
-			choices.append(((_("slot%s - %s (current image), without backup") if x == currentimageslot else _("slot%s - %s, without backup")) % (x, imagedict[x]['imagename']), (x, "without backup"))) 
-		choices.append((_("No, do not flash image"), False))
-		for x in range(1,HIslot):
 			choices.append(((_("slot%s - %s (current image) with, backup") if x == currentimageslot else _("slot%s - %s, with backup")) % (x, imagedict[x]['imagename']), (x, "with backup"))) 
+		choices.append((_("No, do not flash image"), False))
+		if not config.imagemanager.autosettingsbackup.value:
+			for x in range(1,HIslot):
+				choices.append(((_("slot%s - %s (current image), without backup") if x == currentimageslot else _("slot%s - %s, without backup")) % (x, imagedict[x]['imagename']), (x, "without backup"))) 
 		self.session.openWithCallback(self.backupsettings, MessageBox, self.message, list=choices, default=currentimageslot, simple=True)
 
 	def backupsettings(self, retval):
