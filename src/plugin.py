6589f1ea53bc1113057458f31093a8adedf4de4b
Tony Whalley <awhalley@waitrose.com>
2019-11-19 09:10:53 +0100

    Restorewiz - change backup selection to conform to Birdman change

--- src/plugin.py
+++ src/plugin.py
@@ -55,7 +55,7 @@ def checkConfigBackup():
 					files = []
 				if len(files):
 					for file in files:
-						if file.endswith('.tar.gz') and "vix" in file.lower() or file.startswith('%s' %defaultprefix):
+						if file.endswith('.tar.gz') and "vix" in file.lower():
 							list.append((path.join(devpath, file)))
  		if len(list):
 			print '[RestoreWizard] Backup Image:', list[0]
