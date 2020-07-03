a145586609417f629f4b94d507402bdb401b45d7
twol <awhalley@waitrose.com>
2020-07-03 14:27:43 +0200

    ImageManager - handle no internet access

--- src/ImageManager.py
+++ src/ImageManager.py
@@ -268,10 +268,14 @@ class VIXImageManager(Screen):
 		self.session.openWithCallback(self.setupDone, Setup, "viximagemanager", "SystemPlugins/ViX", self.menu_path, PluginLanguageDomain)
 
 	def doDownload(self):
-		self.choices = [("Local", 1), ("OpenViX", 2), ("OpenATV", 3), ("OpenPli", 4), ("ViXDev", 5)]
-		self.urlchoices = [config.imagemanager.imagefeed_User.value, config.imagemanager.imagefeed_ViX.value, config.imagemanager.imagefeed_ATV.value, config.imagemanager.imagefeed_Pli.value, config.imagemanager.imagefeed_Dev.value]
-		self.message = _("Do you want to change download url")
-		self.session.openWithCallback(self.doDownload2, MessageBox, self.message, list=self.choices, default=1, simple=True)
+		try:
+			urlopen('http://google.co.uk', timeout=1)
+			self.choices = [("OpenViX", 1), ("OpenATV", 2), ("OpenPli",3)]
+			self.urlchoices = [config.imagemanager.imagefeed_ViX.value, config.imagemanager.imagefeed_ATV.value, config.imagemanager.imagefeed_Pli.value]
+			self.message = _("Do you want to change download url")
+			self.session.openWithCallback(self.doDownload2, MessageBox, self.message, list=self.choices, default=1, simple=True)
+		except:
+			self.session.open(MessageBox, _("No internet connection detected!"), MessageBox.TYPE_INFO, timeout=10)
 
 	def doDownload2(self, retval):
 		if retval:
