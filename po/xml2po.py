a8a37974b0d0831440835a42240c31fbb382263b
Tony Whalley <tonywhalley@t-online.de>
2020-07-17 14:00:14 +0200

    xml2po - fix print and /n replacement as per ViX main

--- po/xml2po.py
+++ po/xml2po.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import six
+
 import sys
 import os
-import string
 import re
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler, property_lexical_handler
-
 try:
 	from _xmlplus.sax.saxlib import LexicalHandler
-
 	no_comments = False
 except ImportError:
 	class LexicalHandler:
 		def __init__(self):
 			pass
-
 	no_comments = True
-
 
 class parseXML(ContentHandler, LexicalHandler):
 	def __init__(self, attrlist):
@@ -26,19 +25,18 @@ class parseXML(ContentHandler, LexicalHandler):
 		self.ishex = re.compile('#[0-9a-fA-F]+\Z')
 
 	def comment(self, comment):
-		if comment.find("TRANSLATORS:") != -1:
+		if "TRANSLATORS:" in comment:
 			self.last_comment = comment
 
 	def startElement(self, name, attrs):
 		for x in ["text", "title", "value", "caption", "summary", "description"]:
 			try:
-				k = str(attrs[x])
+				k = six.ensure_str(attrs[x])
 				if k.strip() != "" and not self.ishex.match(k):
-					attrlist.add((attrs[x], self.last_comment))
+					attrlist.add((k, self.last_comment))
 					self.last_comment = None
 			except KeyError:
 				pass
-
 
 parser = make_parser()
 
@@ -61,20 +59,13 @@ for arg in sys.argv[1:]:
 	attrlist.sort(key=lambda a: a[0])
 
 	for (k, c) in attrlist:
-		print
-		print '#: ' + arg
-# We need to escape all "s that we have in the string.
-# This replace() *must* come before anything that adds "s!! (such as the
-# newline-handling code).
-# Ideally we'd do this "intelligently" (check for no current preceding \)
-# but there is no reason for there ever to be one, so....
-#
-		k = str(k).replace('"', '\\"')
-		k = k.replace("\\n", "\"\n\"")
+		print()
+		print('#: ' + arg)
+		k.replace("\\n", "\"\n\"")
 		if c:
 			for l in c.split('\n'):
-				print "#. ", l
-		print 'msgid "' + k + '"'
-		print 'msgstr ""'
+				print("#. ", l)
+		print('msgid "' + six.ensure_str(k) + '"')
+		print('msgstr ""')
 
 	attrlist = set()
